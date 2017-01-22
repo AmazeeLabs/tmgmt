a7ec825624db86528aafeeaca6cc30a40190f8b1
mbovan <mbovan@3149657.no-reply.drupal.org>
2017-01-22 16:08:52 +0100

    Issue #2830011 by mbovan: HTML file format doesn't respect $is_file flag

--- translators/tmgmt_file/src/Plugin/tmgmt_file/Format/Html.php
+++ translators/tmgmt_file/src/Plugin/tmgmt_file/Format/Html.php
@@ -61,7 +61,7 @@ class Html implements FormatInterface {
    */
   public function import($imported_file, $is_file = TRUE) {
     $dom = new \DOMDocument();
-    $dom->loadHTMLFile($imported_file);
+    $is_file ? $dom->loadHTMLFile($imported_file) : $dom->loadHTML($imported_file);
     $xml = simplexml_import_dom($dom);
 
     $data = array();
