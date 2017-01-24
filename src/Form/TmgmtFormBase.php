b0bcac6bca02faca0269b12e6321697fe4f291b1
Sascha Grossenbacher <saschagros@gmail.com>
2017-01-24 15:33:56 +0100

    Revert "Issue #2840331 by Berdir: Missing parent call in TmgmtFormBase::__constuctor"
    This reverts commit 691bbcc11a285d442aa26a92784db4349a385638.

--- src/Form/TmgmtFormBase.php
+++ src/Form/TmgmtFormBase.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\tmgmt\Form;
 
-use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\tmgmt\SourceManager;
 use Drupal\tmgmt\TranslatorManager;
@@ -44,10 +42,6 @@ class TmgmtFormBase extends ContentEntityForm {
    *
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager service.
-   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
-   *   The entity type bundle service.
-   * @param \Drupal\Component\Datetime\TimeInterface $time
-   *   The time service.
    * @param \Drupal\tmgmt\TranslatorManager $translator_manager
    *   The translator plugin manager.
    * @param \Drupal\tmgmt\SourceManager $source_manager
@@ -55,8 +49,8 @@ class TmgmtFormBase extends ContentEntityForm {
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer.
    */
-  public function __construct(EntityManagerInterface $entity_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, TranslatorManager $translator_manager, SourceManager $source_manager, RendererInterface $renderer) {
-    parent::__construct($entity_manager, $entity_type_bundle_info, $time);
+  public function __construct(EntityManagerInterface $entity_manager, TranslatorManager $translator_manager, SourceManager $source_manager, RendererInterface $renderer) {
+    $this->entityManager = $entity_manager;
     $this->translatorManager = $translator_manager;
     $this->sourceManager = $source_manager;
     $this->renderer = $renderer;
@@ -68,8 +62,6 @@ class TmgmtFormBase extends ContentEntityForm {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity.manager'),
-      $container->get('entity_type.bundle.info'),
-      $container->get('datetime.time'),
       $container->get('plugin.manager.tmgmt.translator'),
       $container->get('plugin.manager.tmgmt.source'),
       $container->get('renderer')
